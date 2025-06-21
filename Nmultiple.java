e747210dc7237156cc23d6677b341df3a3d53d2d
LIKITH G <118548660+Likithgovind@users.noreply.github.com>
2025-06-21 10:40:49 +0530

    Create Nmultiple.java

---
+++ Nmultiple.java
@@ -0,0 +1,13 @@
+public class Nmultiple
+{
+    public static void main (String [] args)
+    {
+        int n=10;
+        int count=0;
+        for (int i=1;i<=n;i++)
+        {
+            count+=i;
+        }
+        System.out.println(count*n);
+    }
+}
